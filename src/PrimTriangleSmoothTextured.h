94f11b016bf969381602dced043000c6df075ecd
Ashray Adhikari <adhikariashray@gmail.com>
2019-11-08 02:23:18 +0100

    Update PrimTriangleSmoothTextured.h

--- src/PrimTriangleSmoothTextured.h
+++ src/PrimTriangleSmoothTextured.h
@@ -21,7 +21,8 @@ public:
 		// assume u/v coordinates in ray correspond to beta(u) and gamma(v) barycentric coordinates of 
 		// hitpoint on triangle (have to be stored like this in the intersection code !)
 		// --- PUT YOUR CODE HERE ---
-		Vec2f uv = ray.u * m_ta   + ray.v * m_tb + (1 - ray.u - ray.v) * m_tc;
+		Vec2f uv = (1 - ray.u - ray.v) * m_ta   + ray.u * m_tb + ray.v * m_tc;
+		return uv;
 		return Vec2f(0, 0);
 	}
 
